c818472e6645ade48bf6c61f833132d5f7ce9d76
ben <benj.moreau33@gmail.com>
2019-06-20 19:29:25 +0200

    menu network

--- src/app/network/network-routing.module.ts
+++ src/app/network/network-routing.module.ts
@@ -6,7 +6,9 @@ import { NetworkComponent } from './network.component';
 
 const routes: Routes = [
   // Module is lazy loaded, see app-routing.module.ts
-  { path: '', component: NetworkComponent, data: { title: extract('network') } }
+  { path: '', component: NetworkComponent, data: { title: extract('network') } },
+  { path: 'topology', component: NetworkComponent, data: { title: extract('network.topo') } },
+  { path: 'energy-level', component: NetworkComponent, data: { title: extract('network.energy') } }
 ];
 
 @NgModule({
