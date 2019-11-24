c18e89aa4dec327ef3c7e1bed7d636ddb24791b6
ben <benj.moreau33@gmail.com>
2019-11-24 13:02:12 +0100

    chart area

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -79,9 +79,9 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   constructor(
     private apiService: ApiService,
-    private translate: TranslateService,
     private toppy: Toppy,
-    private versionService: VersionService
+    private versionService: VersionService,
+    private translateService: TranslateService
   ) {}
 
   ngOnInit() {
@@ -93,61 +93,61 @@ export class DashboardComponent implements OnInit, OnDestroy {
     this.apiService.getPluginStats().subscribe(res => {
       this.pluginStats = res;
       this.createChart();
-      this.totalTraficSent.label = this.translate.instant('dashboard.trafic.total.trafic.sent');
+      this.totalTraficSent.label = this.translateService.instant('dashboard.trafic.total.trafic.sent');
       this.totalTraficSent.total = res.Sent;
-      this.totalTraficRetx.label = this.translate.instant('dashboard.trafic.retx');
+      this.totalTraficRetx.label = this.translateService.instant('dashboard.trafic.retx');
       this.totalTraficRetx.total = ((res.ReTx / res.Sent) * 100).toFixed(0);
       this.totalTraficRetx.append = '%';
-      this.totalTraficReceived.label = this.translate.instant('dashboard.trafic.total.trafic.received');
+      this.totalTraficReceived.label = this.translateService.instant('dashboard.trafic.total.trafic.received');
       this.totalTraficReceived.total = res.Received;
-      this.totalTraficCluster.label = this.translate.instant('dashboard.trafic.cluster');
+      this.totalTraficCluster.label = this.translateService.instant('dashboard.trafic.cluster');
       this.totalTraficCluster.total = ((res.Cluster / res.Received) * 100).toFixed(0);
       this.totalTraficCluster.append = '%';
-      this.totalTraficCrc.label = this.translate.instant('dashboard.trafic.crc');
+      this.totalTraficCrc.label = this.translateService.instant('dashboard.trafic.crc');
       this.totalTraficCrc.total = ((res.CRC / res.Received) * 100).toFixed(0);
       this.totalTraficCrc.append = '%';
-      this.maxLoad.label = this.translate.instant('dashboard.trafic.maxload');
+      this.maxLoad.label = this.translateService.instant('dashboard.trafic.maxload');
       this.maxLoad.total = res.MaxLoad;
-      this.currentLoad.label = this.translate.instant('dashboard.trafic.currentload');
+      this.currentLoad.label = this.translateService.instant('dashboard.trafic.currentload');
       this.currentLoad.total = res.CurrentLoad;
       this.advancedPieLoad = [
-        { name: this.translate.instant('dashboard.trafic.maxload'), value: res.MaxLoad - res.CurrentLoad },
-        { name: this.translate.instant('dashboard.trafic.currentload'), value: res.CurrentLoad }
+        { name: this.translateService.instant('dashboard.trafic.maxload'), value: res.MaxLoad - res.CurrentLoad },
+        { name: this.translateService.instant('dashboard.trafic.currentload'), value: res.CurrentLoad }
       ];
       this.advancedPieSent = [
-        { name: this.translate.instant('dashboard.trafic.total.trafic.sent'), value: res.Sent - res.ReTx },
-        { name: this.translate.instant('dashboard.trafic.retx'), value: res.ReTx }
+        { name: this.translateService.instant('dashboard.trafic.total.trafic.sent'), value: res.Sent - res.ReTx },
+        { name: this.translateService.instant('dashboard.trafic.retx'), value: res.ReTx }
       ];
       this.advancedPieReceived = [
         {
-          name: this.translate.instant('dashboard.trafic.total.trafic.received'),
+          name: this.translateService.instant('dashboard.trafic.total.trafic.received'),
           value: res.Received - res.Cluster - res.CRC
         },
-        { name: this.translate.instant('dashboard.trafic.cluster'), value: res.Cluster },
-        { name: this.translate.instant('dashboard.trafic.crc'), value: res.CRC }
+        { name: this.translateService.instant('dashboard.trafic.cluster'), value: res.Cluster },
+        { name: this.translateService.instant('dashboard.trafic.crc'), value: res.CRC }
       ];
     });
 
     this.apiService.getZDevices().subscribe(devices => {
       this.devices = devices;
       this.devices.total = this.devices.length;
-      this.devices.label = this.translate.instant('devices');
+      this.devices.label = this.translateService.instant('devices');
       this.routers = this.devices.filter((router: any) => router.LogicalType === 'Router');
-      this.routers.label = this.translate.instant('dashboard.devices.routers');
+      this.routers.label = this.translateService.instant('dashboard.devices.routers');
       this.routers.total = ((this.routers.length / this.devices.total) * 100).toFixed(0);
       this.routers.append = '%';
       this.inDbs = this.devices.filter((router: any) => router.Status === 'inDB');
-      this.inDbs.label = this.translate.instant('dashboard.devices.indb');
+      this.inDbs.label = this.translateService.instant('dashboard.devices.indb');
       this.inDbs.total = ((this.inDbs.length / this.devices.total) * 100).toFixed(0);
       this.inDbs.append = '%';
       this.healthsLive = this.devices.filter((device: any) => device.Health === 'Live');
-      this.healthsLive.label = this.translate.instant('dashboard.devices.live');
+      this.healthsLive.label = this.translateService.instant('dashboard.devices.live');
       this.healthsLive.total = ((this.healthsLive.length / this.devices.total) * 100).toFixed(0);
       this.healthsLive.append = '%';
       this.healthsNotSeen = this.devices.filter((device: any) => {
         return device.Health === 'Not seen last 24hours' || device.Health === 'Not Reachable';
       });
-      this.healthsNotSeen.label = this.translate.instant('dashboard.devices.notseen');
+      this.healthsNotSeen.label = this.translateService.instant('dashboard.devices.notseen');
       this.healthsNotSeen.total = ((this.healthsNotSeen.length / this.devices.total) * 100).toFixed(0);
       this.healthsNotSeen.append = '%';
       this.healthsOthers = this.devices.filter((device: any) => {
@@ -155,29 +155,29 @@ export class DashboardComponent implements OnInit, OnDestroy {
           device.Health !== 'Not seen last 24hours' && device.Health !== 'Not Reachable' && device.Health !== 'Live'
         );
       });
-      this.healthsOthers.label = this.translate.instant('dashboard.devices.enddevice');
+      this.healthsOthers.label = this.translateService.instant('dashboard.devices.enddevice');
       this.healthsOthers.total = ((this.healthsOthers.length / this.devices.total) * 100).toFixed(0);
       this.healthsOthers.append = '%';
       this.advancedPieDevice = [
         {
-          name: this.translate.instant('dashboard.devices.routers'),
+          name: this.translateService.instant('dashboard.devices.routers'),
           value: this.routers.length
         },
         {
-          name: this.translate.instant('dashboard.devices.enddevice'),
+          name: this.translateService.instant('dashboard.devices.enddevice'),
           value: this.devices.length - this.routers.length
         }
       ];
       this.advancedPieState = [
         {
-          name: this.translate.instant('dashboard.devices.live'),
+          name: this.translateService.instant('dashboard.devices.live'),
           value: this.healthsLive.length
         },
         {
-          name: this.translate.instant('dashboard.devices.notseen'),
+          name: this.translateService.instant('dashboard.devices.notseen'),
           value: this.healthsNotSeen.length
         },
-        { name: this.translate.instant('dashboard.devices.others'), value: this.healthsOthers.length }
+        { name: this.translateService.instant('dashboard.devices.others'), value: this.healthsOthers.length }
       ];
       this.devicesOnBattery = devices.filter((device: any) => device.LogicalType !== 'Router');
       const _batteryInf30 = this.devicesOnBattery.filter((device: any) => device.Battery < 30);
@@ -187,34 +187,34 @@ export class DashboardComponent implements OnInit, OnDestroy {
       this.batterySup50 = this.devices.filter((it: any) => _batterySup50.find((iter: any) => iter.IEEE === it.IEEE));
       this.batterySup30 = this.devices.filter((it: any) => _batterySup30.find((iter: any) => iter.IEEE === it.IEEE));
       this.batteryInf30.totalDevices = this.devicesOnBattery.length;
-      this.batteryInf30.label = this.translate.instant('dashboard.devices.battery.inf.30');
+      this.batteryInf30.label = this.translateService.instant('dashboard.devices.battery.inf.30');
       this.batteryInf30.total = this.batteryInf30.length;
-      this.batteryInf30.append = this.translate.instant('devices');
-      this.batterySup30.label = this.translate.instant('dashboard.devices.battery.sup.30');
+      this.batteryInf30.append = this.translateService.instant('devices');
+      this.batterySup30.label = this.translateService.instant('dashboard.devices.battery.sup.30');
       this.batterySup30.total = this.batterySup30.length;
-      this.batterySup30.append = this.translate.instant('devices');
-      this.batterySup50.label = this.translate.instant('dashboard.devices.battery.sup.50');
+      this.batterySup30.append = this.translateService.instant('devices');
+      this.batterySup50.label = this.translateService.instant('dashboard.devices.battery.sup.50');
       this.batterySup50.total = this.batterySup50.length;
-      this.batterySup50.append = this.translate.instant('devices');
+      this.batterySup50.append = this.translateService.instant('devices');
       this.advancedPieBattery = [
         {
-          name: this.translate.instant('dashboard.devices.battery.inf.30'),
+          name: this.translateService.instant('dashboard.devices.battery.inf.30'),
           value: this.batteryInf30.length
         },
         {
-          name: this.translate.instant('dashboard.devices.battery.sup.30'),
+          name: this.translateService.instant('dashboard.devices.battery.sup.30'),
           value: this.batterySup30.length
         },
-        { name: this.translate.instant('dashboard.devices.battery.sup.50'), value: this.batterySup50.length }
+        { name: this.translateService.instant('dashboard.devices.battery.sup.50'), value: this.batterySup50.length }
       ];
     });
 
-    this.advancedPieLoadLabel = this.translate.instant('dashboard.trafic.maxload.label');
-    this.advancedPieSentLabel = this.translate.instant('dashboard.trafic.total.trafic.sent.label');
-    this.advancedPieReceivedLabel = this.translate.instant('dashboard.trafic.total.trafic.received.label');
-    this.advancedPieDeviceLabel = this.translate.instant('dashboard.devices.label');
-    this.advancedPieStateLabel = this.translate.instant('dashboard.devices.state.label');
-    this.advancedPieBatteryLabel = this.translate.instant('dashboard.devices.battery.label');
+    this.advancedPieLoadLabel = this.translateService.instant('dashboard.trafic.maxload.label');
+    this.advancedPieSentLabel = this.translateService.instant('dashboard.trafic.total.trafic.sent.label');
+    this.advancedPieReceivedLabel = this.translateService.instant('dashboard.trafic.total.trafic.received.label');
+    this.advancedPieDeviceLabel = this.translateService.instant('dashboard.devices.label');
+    this.advancedPieStateLabel = this.translateService.instant('dashboard.devices.state.label');
+    this.advancedPieBatteryLabel = this.translateService.instant('dashboard.devices.battery.label');
   }
 
   percentageFormatting(value: any) {
@@ -272,7 +272,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
       },
       yAxis: {
         title: {
-          text: this.translate.instant('dashboard.trend.chart.yaxis')
+          text: this.translateService.instant('dashboard.trend.chart.yaxis')
         },
         labels: {
           formatter: function() {
@@ -302,19 +302,19 @@ export class DashboardComponent implements OnInit, OnDestroy {
       },
       series: [
         {
-          name: 'Txps',
+          name: this.translateService.instant('dashboard.tx'),
           data: tx,
           type: 'area',
           color: 'blue'
         },
         {
-          name: 'Rxps',
+          name: this.translateService.instant('dashboard.rx'),
           data: rx,
           type: 'area',
           color: 'green'
         },
         {
-          name: 'Load',
+          name: this.translateService.instant('dashboard.load'),
           data: load,
           type: 'area',
           color: 'orange'
@@ -329,23 +329,23 @@ export class DashboardComponent implements OnInit, OnDestroy {
   open(name: string, event: any) {
     let devices;
     if (name === 'device') {
-      if (event.name === this.translate.instant('dashboard.devices.routers')) {
+      if (event.name === this.translateService.instant('dashboard.devices.routers')) {
         devices = this.routers;
       } else {
         devices = this.devicesOnBattery;
       }
     } else if (name === 'state') {
-      if (event.name === this.translate.instant('dashboard.devices.live')) {
+      if (event.name === this.translateService.instant('dashboard.devices.live')) {
         devices = this.healthsLive;
-      } else if (event.name === this.translate.instant('dashboard.devices.others')) {
+      } else if (event.name === this.translateService.instant('dashboard.devices.others')) {
         devices = this.healthsOthers;
       } else {
         devices = this.healthsNotSeen;
       }
     } else if (name === 'battery') {
-      if (event.name === this.translate.instant('dashboard.devices.battery.inf.30')) {
+      if (event.name === this.translateService.instant('dashboard.devices.battery.inf.30')) {
         devices = this.batteryInf30;
-      } else if (event.name === this.translate.instant('dashboard.devices.battery.sup.30')) {
+      } else if (event.name === this.translateService.instant('dashboard.devices.battery.sup.30')) {
         devices = this.batterySup30;
       } else {
         devices = this.batterySup50;
