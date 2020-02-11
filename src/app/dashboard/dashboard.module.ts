8112e5348be4c6f26e3412f21692c019c64f45bf
ben33880 <benj.moreau33@gmail.com>
2020-02-11 23:04:55 +0100

    angular 9

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { NgxGaugeModule } from 'ngx-gauge';
 import { ToppyModule } from 'toppy';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
@@ -14,6 +13,7 @@ import * as exporting from 'highcharts/modules/exporting.src';
 import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxGaugeModule } from '@ben33880/gauge';
 
 @NgModule({
   imports: [
