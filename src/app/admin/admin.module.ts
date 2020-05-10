708b0c1560eefdbdd12b4d47ff8b3e9ec9c2a03c
ben33880 <benj.moreau33@gmail.com>
2020-05-10 16:26:53 +0200

    Provisioning on a specific router #22

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@app/shared';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { SharedModule } from '@app/shared';
-import { PermitToJoinComponent } from './permit-to-join/permit-to-join.component';
-import { SwResetComponent } from './sw-reset/sw-reset.component';
+import { AssistProvisionningComponent } from './assist-provisionning/assist-provisionning.component';
 import { ErasePdmComponent } from './erase-pdm/erase-pdm.component';
+import { PermitToJoinRouterComponent } from './permit-to-join-router/permit-to-join-router.component';
+import { PermitToJoinComponent } from './permit-to-join/permit-to-join.component';
 import { ReloadPluginComponent } from './reload-plugin/reload-plugin.component';
 import { RescanGroupComponent } from './rescan-group/rescan-group.component';
-import { AssistProvisionningComponent } from './assist-provisionning/assist-provisionning.component';
+import { SwResetComponent } from './sw-reset/sw-reset.component';
 
 @NgModule({
   imports: [AdminRoutingModule, SharedModule],
   declarations: [
     AdminComponent,
     PermitToJoinComponent,
+    PermitToJoinRouterComponent,
     SwResetComponent,
     ErasePdmComponent,
     ReloadPluginComponent,
---
+++ src/app/admin/permit-to-join-router/permit-to-join-router.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Logger } from '@app/core';
+import { ApiService } from '@app/services/api.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ToastrService } from 'ngx-toastr';
+import { forkJoin } from 'rxjs';
+import { DeviceByName } from '@app/shared/models/device-by-name';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+
+const log = new Logger('PermitToJoinRouterComponent');
+
+@Component({
+  selector: 'app-permit-to-join-router',
+  templateUrl: './permit-to-join-router.component.html',
+  styleUrls: ['./permit-to-join-router.component.scss']
+})
+export class PermitToJoinRouterComponent implements OnInit {
+  permitToJoin: any;
+  routers: DeviceByName[];
+  form: FormGroup;
+
+  constructor(
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private translate: TranslateService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      deviceSelected: [null, Validators.required]
+    });
+
+    forkJoin([this.apiService.getPermitToJoin(), this.apiService.getZDevices()]).subscribe(
+      ([permitToJoin, devices]) => {
+        this.permitToJoin = permitToJoin;
+        this.routers = devices.filter((router: any) => router.LogicalType === 'Router');
+      }
+    );
+  }
+
+  updatePermitToJoin(value: number) {
+    this.permitToJoin.PermitToJoin = value;
+    this.permitToJoin.Router = (this.form.get('deviceSelected').value as DeviceByName)._NwkId;
+
+    this.apiService.putPermitToJoin(this.permitToJoin).subscribe((result: any) => {
+      this.toastr.success(this.translate.instant('api.global.succes.update.title'));
+    });
+  }
+}
