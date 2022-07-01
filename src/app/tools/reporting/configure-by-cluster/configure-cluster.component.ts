abd535647224bf6115e1d8bda6b5e78717226a93
ben33880 <benj.moreau33@gmail.com>
2022-07-01 15:24:44 +0000

    prise en compte datatype

--- src/app/tools/reporting/configure-by-cluster/configure-cluster.component.ts
+++ src/app/tools/reporting/configure-by-cluster/configure-cluster.component.ts
@@ -20,6 +20,32 @@ export class ConfigureByClusterReportingComponent implements OnChanges {
 
   clustersToDisplay: ClusterToDisplay[] = [];
 
+  datatypeConvertor: any = [
+    { type: '20', longueur: '0' },
+    { type: '10', longueur: '0' },
+    { type: '21', longueur: '00' },
+    { type: '22', longueur: '000' },
+    { type: '23', longueur: '0000' },
+    { type: '24', longueur: '00000' },
+    { type: '25', longueur: '000000' },
+    { type: '26', longueur: '0000000' },
+    { type: '27', longueur: '00000000' },
+    { type: '28', longueur: '0' },
+    { type: '29', longueur: '00' },
+    { type: '2a', longueur: '000' },
+    { type: '2b', longueur: '0000' },
+    { type: '2c', longueur: '00000' },
+    { type: '2d', longueur: '000000' },
+    { type: '2e', longueur: '0000000' },
+    { type: '2f', longueur: '00000000' },
+    { type: '38', longueur: '00' },
+    { type: '39', longueur: '0000' },
+    { type: '3a', longueur: '00000000' },
+    { type: 'e0', longueur: '0000' },
+    { type: 'e1', longueur: '0000' },
+    { type: 'e2', longueur: '0000' }
+  ];
+
   constructor(
     private toastr: ToastrService,
     private apiService: ApiService,
@@ -60,9 +86,20 @@ export class ConfigureByClusterReportingComponent implements OnChanges {
     } else {
       const rowUpdated = this.clusters
         .find(cluster => cluster.ClusterId === clusterToDisplay.clusterId)
-        .Attributes.find(attribute => attribute.Attribute === clusterToDisplay.attributeId)
-        .Infos.find(infos => infos.DataType === clusterToDisplay.dataType);
-      rowUpdated[col] = Number(value).toString(16).toUpperCase();
+        .Attributes.find(attribute => attribute.Attribute === clusterToDisplay.attributeId).Infos[0];
+      if (col === 'Change') {
+        const type = this.datatypeConvertor.find((datatype: any) => datatype.type === rowUpdated.DataType);
+        if (type.type === '10' && value > 1) {
+          this.clustersChange.emit(null);
+          alert(this.translate.instant('reporting.configure.length.error'));
+        }
+        const longueur = type.longueur;
+        rowUpdated[col] = (type.longueur + Number(value).toString(16).toUpperCase()).slice(
+          (type.longueur as string).length
+        );
+      } else {
+        rowUpdated[col] = Number(value).toString(16).toUpperCase();
+      }
       this.clustersChange.emit(this.clusters);
     }
   }
